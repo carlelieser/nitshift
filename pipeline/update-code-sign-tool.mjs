9405d63d9120214513904e9262711bf1d1fd073e
Carlos <carloseliesersantos@gmail.com>
2024-01-31 00:27:23 -0500

    add script for fetching latest code sign tool from GitHub

---
+++ pipeline/update-code-sign-tool.mjs
@@ -0,0 +1,40 @@
+import { Octokit } from "octokit";
+import * as keys from "../src/main/keys.ts";
+import fetch from "node-fetch";
+import path from "node:path";
+import * as fs from "node:fs";
+import { fileURLToPath } from "url";
+import { Extract } from "unzip";
+import ora from "ora";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const spinner = ora();
+const target = path.join(__dirname, "..", "code-sign-tool");
+const octokit = new Octokit({
+	auth: keys.git,
+});
+
+spinner.start();
+
+spinner.info(`Cleaning directory: ${target}`)
+
+await fs.promises.rm(target, { force: true, recursive: true });
+
+spinner.info("Getting latest release asset from GitHub...");
+
+const release = await octokit.rest.repos.getLatestRelease({
+	owner: "SSLcom",
+	repo: "CodeSignTool",
+});
+
+const asset = release.data.assets.find(asset => asset.name.includes("windows"));
+
+spinner.succeed(`Found: ${asset.browser_download_url}`);
+
+const response = await fetch(asset.browser_download_url);
+
+spinner.start("Downloading and extracting zip...")
+
+response.body.pipe(Extract({ path: target })).on("finish", () => spinner.succeed(`CodeSignTool updated to ${release.data.tag_name}!`));
