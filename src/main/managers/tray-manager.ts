bfcba8837b7ccf238135cb7586a2492d255f5302
Carlos <carloseliesersantos@gmail.com>
2024-02-02 05:17:18 -0500

    change tray icon based on theme

--- src/main/managers/tray-manager.ts
+++ src/main/managers/tray-manager.ts
@@ -1,53 +1,80 @@
 import iconPath from "@assets/img/icon-alt.png?asset";
+import iconDarkPath from "@assets/img/icon-alt-dark.png?asset";
 import { isDev } from "@common/utils";
-import { app, clipboard, Menu, nativeImage, Tray } from "electron";
+import { app, clipboard, Menu, nativeImage, nativeTheme, Tray } from "electron";
 import process from "process";
 import release from "@common/release.json";
 import { machineIdSync } from "node-machine-id";
 import EventEmitter from "events";
 
-const nativeIcon = nativeImage.createFromPath(iconPath).resize({ width: 32 });
-
 class TrayManager extends EventEmitter {
 	private tray: Tray;
+	private icon: Electron.NativeImage;
+	private menu: Electron.Menu;
+
+	constructor() {
+		super();
+		nativeTheme.on("updated", this.updateTrayIcon);
+	}
 
 	public create = () => {
-		this.tray = new Tray(nativeIcon);
-		const devLabel = isDev ? "DEV" : "";
-		const contextMenu = Menu.buildFromTemplate([
-			{
-				icon: nativeIcon.resize({ width: 16 }),
-				label: "Glimmr",
-				sublabel: `${release.tag_name} ${devLabel}`,
-				enabled: false
-			},
-			{
-				type: "separator"
-			},
-			{
-				label: "Copy Support ID",
-				click: () => clipboard.writeText(machineIdSync())
-			},
-			{
-				label: "Check for Updates...",
-				click: () => this.emit("check-for-updates")
-			},
-			{
-				type: "separator"
-			},
-			{
-				label: "Quit Glimmr",
-				click: () => {
-					this.tray.destroy();
-					app.quit();
-					process.exit();
-				}
-			}
-		]);
+		this.updateIcon();
+		this.updateTray();
+		this.updateMenu();
 		this.tray.setToolTip("Glimmr");
-		this.tray.setContextMenu(contextMenu);
-
 		this.tray.on("click", () => this.emit("click"));
+	};
+
+	private getMenuTemplate = (): (Electron.MenuItem | Electron.MenuItemConstructorOptions)[] => [
+		{
+			icon: this.icon.resize({ width: 16 }),
+			label: "Glimmr",
+			sublabel: `${release.tag_name} ${isDev ? "DEV" : ""}`,
+			enabled: false
+		},
+		{
+			type: "separator"
+		},
+		{
+			label: "Copy Support ID",
+			click: () => clipboard.writeText(machineIdSync())
+		},
+		{
+			label: "Check for Updates...",
+			click: () => this.emit("check-for-updates")
+		},
+		{
+			type: "separator"
+		},
+		{
+			label: "Quit Glimmr",
+			click: () => {
+				this.tray.destroy();
+				app.quit();
+				process.exit();
+			}
+		}
+	];
+
+	private updateTray = () => {
+		this.tray = new Tray(this.icon);
+	};
+
+	private updateMenu = () => {
+		this.menu = Menu.buildFromTemplate(this.getMenuTemplate());
+		this.tray.setContextMenu(this.menu);
+	};
+
+	private updateIcon = () => {
+		this.icon = nativeImage
+			.createFromPath(nativeTheme.shouldUseDarkColors ? iconPath : iconDarkPath)
+			.resize({ width: 32 });
+	};
+
+	private updateTrayIcon = () => {
+		this.updateIcon();
+		this.tray.setImage(this.icon);
+		this.updateMenu();
 	};
 }
 
