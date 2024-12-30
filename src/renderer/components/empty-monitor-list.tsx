84a1044ddda349f575d6e680842fb42c8dda6991
Carlos <carloseliesersantos@gmail.com>
2024-12-29 23:47:08 -0500

    add empty monitor list to use as fallback during suspense

---
+++ src/renderer/components/empty-monitor-list.tsx
@@ -0,0 +1,26 @@
+import { Paper, Stack } from "@mui/material";
+import { useMemo } from "react";
+import { Skeleton } from "@mui/lab";
+import Slider from "./slider";
+
+const EmptyMonitorList = () => {
+	const arr = useMemo(() => Array.from({ length: 3 }), []);
+	return (
+		<Stack flex={1} gap={2} p={2}>
+			<Paper sx={{ borderRadius: 4 }}>
+				<Slider value={100} disabled={true} />
+			</Paper>
+			{arr.map((_, index) => (
+				<Paper key={`empty-monitor-${index}`} sx={{ p: 2, borderRadius: 4 }}>
+					<Skeleton variant={"text"} />
+					<Skeleton variant={"text"} width={"33%"} />
+					<Stack pt={1}>
+						<Slider value={100} disabled={true} />
+					</Stack>
+				</Paper>
+			))}
+		</Stack>
+	);
+};
+
+export default EmptyMonitorList;
