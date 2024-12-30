1858f88c9a6ff8f9c0f5c8b4da94c55b127872ee
Carlos <carloseliesersantos@gmail.com>
2024-12-29 23:48:24 -0500

    include update check button in settings page

--- src/renderer/components/dialogs/settings/footer.tsx
+++ src/renderer/components/dialogs/settings/footer.tsx
@@ -1,21 +1,32 @@
-import { ButtonGroup, Stack, Typography } from "@mui/material";
+import { Button, ButtonGroup, Stack, Typography } from "@mui/material";
 import ReviewButton from "@buttons/review-button";
 import ReportABug from "@buttons/report-a-bug";
 import EmailUs from "@buttons/email-us";
 import release from "@common/release.json";
 import { isDev } from "@common/utils";
+import { ipcRenderer } from "electron";
+import { Update } from "mui-symbols";
 
 const Footer = () => {
+	const handleCheckForUpdates = () => {
+		ipcRenderer.send("app/check-for-updates");
+	};
+
 	return (
-		<Stack spacing={2} p={2} direction={"column"} width={"100%"} alignItems={"center"} justifyContent={"center"}>
-			<ButtonGroup variant={"outlined"} size={"small"}>
+		<Stack spacing={2} p={2} direction={"column"}>
+			<ButtonGroup fullWidth={true} variant={"outlined"} size={"small"}>
 				<ReportABug />
 				<ReviewButton />
 				<EmailUs />
 			</ButtonGroup>
-			<Typography variant={"caption"} color={"text.secondary"} textTransform={"uppercase"}>
-				Glimmr {isDev ? "DEV" : ""} {release.tag_name}
-			</Typography>
+			<Stack direction={"row"} width={"100%"} alignItems={"center"} justifyContent={"space-between"}>
+				<Typography variant={"caption"} color={"text.secondary"} textTransform={"uppercase"}>
+					Glimmr {isDev ? "DEV" : ""} {release.tag_name}
+				</Typography>
+				<Button startIcon={<Update />} size={"small"} onClick={handleCheckForUpdates}>
+					Check for Updates
+				</Button>
+			</Stack>
 		</Stack>
 	);
 };
