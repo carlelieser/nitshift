2ffd075b680b3ac1af09db87f60cb4fc29427e3f
Carlos <carloseliesersantos@gmail.com>
2024-12-30 00:13:13 -0500

    fix email-us button

---
+++ src/renderer/components/buttons/email-us-button.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@mui/material";
+import { Send } from "mui-symbols";
+import { loadUserId } from "../../storage";
+import { shell } from "electron";
+import { MouseEventHandler } from "react";
+
+const EmailUsButton = () => {
+	const handleOpenEmail: MouseEventHandler<HTMLButtonElement> = (e) => {
+		e.preventDefault();
+		e.stopPropagation();
+		shell.openExternal(`mailto:support@glimmr.app?subject=[ID: ${loadUserId()}]`);
+	};
+
+	return (
+		<Button startIcon={<Send />}
+				color={"success"} onClick={handleOpenEmail}>
+			Email Us
+		</Button>
+	);
+};
+
+export default EmailUsButton;
