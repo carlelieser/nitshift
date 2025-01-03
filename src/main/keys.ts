b2e518ab5c338881dcf656925dd256901270bbe2
Carlos <carloseliesersantos@gmail.com>
2025-01-03 03:44:50 -0500

    remove git api key, replace octokit usage with fetch call to api

--- src/main/keys.ts
+++ src/main/keys.ts
@@ -1,4 +1,3 @@
-export const git = "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";
 export const encryption = "740ca4d3deb34816964febc571a8dcda90dd96f2ea95adbc79d91838add2ef2b";
 export const version_id = "version-id";
 export const main_id = "project-hash";
