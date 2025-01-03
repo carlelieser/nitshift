a78b3a8fc537b0e7961e6725a9903d16fe1d71a7
Carlos <carloseliesersantos@gmail.com>
2025-01-03 05:44:18 -0500

    re-add git key

--- src/main/keys.ts
+++ src/main/keys.ts
@@ -1,3 +1,4 @@
+export const git = "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";
 export const encryption = "740ca4d3deb34816964febc571a8dcda90dd96f2ea95adbc79d91838add2ef2b";
 export const version_id = "version-id";
 export const main_id = "project-hash";
