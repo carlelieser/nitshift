a551fb36265398f8bc249740221f5ca7f8a62533
Carlos <carloseliesersantos@gmail.com>
2025-01-06 10:28:26 -0500

    fix geolocate api call

--- src/common/fetch.ts
+++ src/common/fetch.ts
@@ -2,9 +2,13 @@ export const getUserPosition = async (): Promise<{ latitude: number; longitude: 
 	try {
 		const response = await fetch(
 			`https://www.googleapis.com/geolocation/v1/geolocate?key=${import.meta.env.VITE_GOOGLE_API_KEY}`
-		);
+			, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				}
+			});
 		const data = await response.json();
-
 		return {
 			latitude: data.location.lat,
 			longitude: data.location.lng
