248ba2192a92801fdcf33686f63bd93997062252
Carlos <carloseliesersantos@gmail.com>
2025-01-10 17:58:41 -0500

    change script extension based on platform

--- pipeline/sign.js
+++ pipeline/sign.js
@@ -7,11 +7,13 @@ const ora = require("ora");
 const logSymbols = require("log-symbols");
 const path = require("path");
 const fg = require("fast-glob");
-
+const os = require("os");
+const isWindows = os.platform() === "win32";
+const scriptExtension = isWindows ? ".bat" : ".sh";
 const root = path.resolve(__dirname, "..");
 const installer = path.join(root, "dist", "squirrel-windows", "glimmr-setup.exe");
-const tool = path.dirname(fg.globSync("code-sign-tool/**/CodeSignTool.bat", { cwd: root }).shift());
-const command = `./CodeSignTool.bat sign -username=${process.env.SSL_USER} -password=${process.env.SSL_PASS} -totp_secret=${process.env.SSL_TOTP_SECRET} -input_file_path="${installer}"`;
+const tool = path.dirname(fg.globSync(`code-sign-tool/**/CodeSignTool${scriptExtension}`, { cwd: root }).shift());
+const command = `./CodeSignTool${scriptExtension} sign -username=${process.env.SSL_USER} -password=${process.env.SSL_PASS} -totp_secret=${process.env.SSL_TOTP_SECRET} -input_file_path="${installer}"`;
 
 console.log(logSymbols.info, `Running command: ${command}`);
 
