be4b66d7e08848e00a679020bac923926c06a9f8
Carlos <carloseliesersantos@gmail.com>
2025-01-10 14:38:25 -0500

    implement script for modifying user_ids key

---
+++ pipeline/prepare-for-users.mjs
@@ -0,0 +1,17 @@
+import path from "path";
+import fs from "fs-extra";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const userIds = process.argv[2] ?? "*";
+const rootPath = path.join(__dirname, "..");
+const keyPath = path.join(rootPath, "src", "main", "keys.ts");
+const keyContent = await fs.readFile(keyPath, "utf8");
+const keyContentLines = keyContent.split("\n");
+const baseContentLines = keyContentLines.slice(0, keyContentLines.findIndex((line) => line.includes("export const user_ids")));
+const baseContent = baseContentLines.join("\n");
+const newKeyContent = `${baseContent}\nexport const user_ids = [${userIds.split(" ").map(id => `"${id}"`).join(", ")}];\n`;
+
+await fs.outputFile(keyPath, newKeyContent);
