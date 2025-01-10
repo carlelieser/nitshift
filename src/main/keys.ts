b53fb92d7ab4631eaa566535cdeb55bae8d16d70
Carlos <carloseliesersantos@gmail.com>
2025-01-09 23:03:24 -0500

    implement custom license loader to use in user-specific builds

--- src/main/keys.ts
+++ src/main/keys.ts
@@ -2,3 +2,7 @@ export const git = "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";
 export const encryption = "740ca4d3deb34816964febc571a8dcda90dd96f2ea95adbc79d91838add2ef2b";
 export const version_id = "version-id";
 export const main_id = "project-hash";
+export const premium_license = "premium";
+export const free_license = "free";
+
+export const user_ids = ["*"];
